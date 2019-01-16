354cd11b3cf512411c59058e2957143b2e451bc6
Adhithya Srinivasan <adhithyasrinivasan@gmail.com>
2019-01-16 10:38:08 +0530

    Update custom_logger.js

--- custom_logger.js
+++ custom_logger.js
@@ -2,7 +2,9 @@ exports.log = function(msg)
 {
   let message = msg;
   if(message==null)
-	  return console.log("");
-	else
-		return console.log(message);
+  {
+	return console.log("");
+  }
+else{
+return console.log(message);}
 }
